01d0f123858a5519a737fec1270c5bfda395c6ed
jiaxiaojie <1277802574@qq.com>
2018-01-30 17:59:49 +0800

    卡券

--- pages/allorders/allorders.js
+++ pages/allorders/allorders.js
@@ -22,7 +22,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    if (options){
+      this.setData({
+        currentTab: options.tab
+      })
+    }
   },
 
   /**
--- pages/person/person.js
+++ pages/person/person.js
@@ -1,18 +1,50 @@
 // pages/person/person.js
+//获取应用实例
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    remainder:0
+    remainder:0,
+    userInfo: {},
+    hasUserInfo: false
   },
-
+//获取头像
+  getUserAvatar:function(){
+    console.info(app.globalData.userInfo);
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  //充值
+  goReacharge:function(){
+    wx.navigateTo({
+      url: '../recharge/recharge',
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     var that = this;
+    that.getUserAvatar();
     wx.request({
       url: 'http://hotel.chengxu-tec.com/api/order', //仅为示例，并非真实的接口地址
       header: {
